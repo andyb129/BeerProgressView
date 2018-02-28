e5f7d96066679eb86cee6e74166c99f10930e9f3
Mez Pahlan <mez.pahlan@gmail.com>
2018-02-28 21:01:07 +0000

    Feature/convert sample app (#13)
    * Bump up tools and library dependency versions (#12)
    
    Upon opening the project and syncing with AS 3.0.1 we receive
    warnings that the tools declared are too low and should be updated.
    
    Also consume the latest patch level for declared support libraries.
    
    * Add Kotlin support to project
    
    * Remove unused variables from AsyncTask
    
    * Migrate MainActivity to Kotlin
    
    * Bump Kotlin version number
    
    * Replace Java MainActivity with Kotlin version
    
    * Replace Java PourBeerTask with Kotlin version

---
+++ sample/src/main/java/uk/co/barbuzz/beerprogressview/sample/MainActivity.kt
@@ -0,0 +1,110 @@
+package uk.co.barbuzz.beerprogressview.sample
+
+import android.content.Intent
+import android.net.Uri
+import android.os.AsyncTask
+import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.roughike.bottombar.BottomBar
+import com.roughike.bottombar.OnMenuTabClickListener
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    private var pourBeerTask: AsyncTask<Void, Int, Void>? = null
+    private val infoDialogue: AlertDialog by lazy {
+        AlertDialog.Builder(this)
+                .setTitle(R.string.app_name)
+                .setMessage(R.string.info_details)
+                .setCancelable(true)
+                .setPositiveButton("OK", { dialog, _ -> dialog.dismiss() })
+                .setNegativeButton("More Info", { dialog, _ ->
+                    dialog.dismiss()
+                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(resources.getString(R.string.github_link))))
+                })
+                .create()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        initViews(savedInstanceState)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val itemId = item?.itemId
+        when (itemId) {
+            R.id.action_github -> {
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse(resources.getString(R.string.github_link))
+                startActivity(intent)
+                return true
+            }
+            R.id.action_info -> {
+                infoDialogue()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun infoDialogue() {
+        if (infoDialogue.isShowing) {
+            return
+        } else {
+            infoDialogue.show()
+        }
+    }
+
+    private fun initViews(savedInstanceState: Bundle?) {
+        val activity = this
+        val bottomBar = BottomBar.attach(this, savedInstanceState)
+
+        bottomBar.setItemsFromMenu(R.menu.menu_bottom_bar, object : OnMenuTabClickListener {
+            override fun onMenuTabReSelected(menuItemId: Int) {
+            }
+
+            override fun onMenuTabSelected(menuItemId: Int) {
+                // The user selected item
+                when (menuItemId) {
+                    R.id.lager_item -> {
+                        pourBeer(ContextCompat.getColor(activity, R.color.lager),
+                                ContextCompat.getColor(activity, R.color.lager_bubble))
+                    }
+                    R.id.ale_item -> {
+                        pourBeer(ContextCompat.getColor(activity, R.color.ale),
+                                ContextCompat.getColor(activity, R.color.ale_bubble))
+                    }
+                    R.id.stout_item -> {
+                        pourBeer(ContextCompat.getColor(activity, R.color.stout),
+                                ContextCompat.getColor(activity, R.color.stout_bubble))
+                    }
+                }
+            }
+        })
+    }
+
+    fun pourBeer(beerColour: Int, bubbleColour: Int) {
+        if (pourBeerTask != null) {
+            pourBeerTask!!.cancel(true)
+            pourBeerTask = null
+            content_main_beer_progress_view!!.beerProgress = 0
+        }
+        content_main_beer_progress_view!!.beerColor = beerColour
+        content_main_beer_progress_view!!.bubbleColor = bubbleColour
+        pourBeerTask = PourBeerTask(content_main_beer_progress_view).execute()
+    }
+}
